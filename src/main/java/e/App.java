47d1ba017898f073581ec41bd5054d8a867db204
akochetkov <akochetkov@ceki.ru>
2024-03-16 16:07:16 +0300

    O0308 SpringApplication.run(App.class)

--- src/main/java/e/App.java
+++ src/main/java/e/App.java
@@ -1,10 +1,12 @@
 package e;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-      System.out.println("cp0:");
+        System.out.println("cp0:");
+        SpringApplication.run(App.class);
     }
 }
