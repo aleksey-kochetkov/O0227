8da4c89f96e6ab75e7cfc9476538fa946950a5d2
akochetkov <akochetkov@ceki.ru>
2024-03-16 17:44:31 +0300

    O0309 CommandLineRunner#run(String[] args)

--- src/main/java/e/App.java
+++ src/main/java/e/App.java
@@ -2,11 +2,17 @@ package e;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class App {
+public class App implements CommandLineRunner {
+
     public static void main(String[] args) {
-        System.out.println("cp0:");
         SpringApplication.run(App.class);
+    }
+
+    @Override
+    public void run(String[] args) {
+        System.out.println("cp0:");
     }
 }
