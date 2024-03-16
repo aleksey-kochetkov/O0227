612da1863432c043f2c0618fde63678186ecdd66
akochetkov <akochetkov@ceki.ru>
2024-03-16 12:28:10 +0300

    O0307 @SpringBootApplication

--- src/main/java/e/App.java
+++ src/main/java/e/App.java
@@ -1,5 +1,8 @@
 package e;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
       System.out.println("cp0:");
