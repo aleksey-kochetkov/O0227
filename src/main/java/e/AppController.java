7e1b5973cb1b267dc73dd7a985e1fc008eb82aed
akochetkov <akochetkov@ceki.ru>
2024-03-17 19:17:12 +0300

    O0311 TomcatWebServer  : Tomcat started on port(s): 8080 (http) with context path ''

---
+++ src/main/java/e/AppController.java
@@ -0,0 +1,11 @@
+package e;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+    @GetMapping
+    public void get() {
+    }
+}
