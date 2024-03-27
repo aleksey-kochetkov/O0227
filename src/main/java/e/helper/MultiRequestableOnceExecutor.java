df8e8ea320ebf9d30c65e854d3d1ca1a8794f0ae
akochetkov <akochetkov@ceki.ru>
2024-03-27 12:56:39 +0300

    O0314 MultiRequestableOnceExecutor#request()

--- src/main/java/e/helper/MultiRequestableOnceExecutor.java
+++ src/main/java/e/helper/MultiRequestableOnceExecutor.java
@@ -3,8 +3,17 @@ package e.helper;
 import java.util.concurrent.Executor;
 
 public class MultiRequestableOnceExecutor implements Executor {
+    private Runnable command;
+    private Thread thread;
+    
+    public MultiRequestableOnceExecutor(Runnable command) {
+        this.command = command;
+    }
+    
     @Override
     public void execute(Runnable command) {
+        this.thread = new Thread() {};
+        this.thread.start();
     }
     
     public void request() {
